bc9d70267aba222d666cee4d65f14e375fca861c
bilo1967 <17109395+bilo1967@users.noreply.github.com>
2021-05-12 11:00:47 +0200

    Update config.js

--- config/config.js
+++ config/config.js
@@ -120,7 +120,6 @@ const PeerJSConfig = {
             { urls: 'stun:stun.l.google.com:19302' },
             { urls: 'stun:stun1.l.google.com:19302' },
             { urls: 'stun:stun2.l.google.com:19302' },
-//          { urls: 'turn:numb.viagenie.ca', username: 'fuffology@gmail.com', credential: 'pippero'},
 //          { urls: 'turn:numb.viagenie.ca', username: 'webrtc@live.com', credential: 'muazkh'},
 //          { urls: 'turn:0.peerjs.com:3478', username: 'peerjs', credential: 'peerjsp' },
             { urls: 'stun:stun3.l.google.com:19302' },
