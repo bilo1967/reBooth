f014d84724fc602959354fc2d72a63cec16ac846
bilo1967 <17109395+bilo1967@users.noreply.github.com>
2021-09-14 12:09:05 +0200

    Session timeout prevention
    Session timeout prevention

--- session.php
+++ session.php
@@ -1,7 +1,17 @@
 <?php
+    // Load configuration
     include_once(dirname(__FILE__) . "/config/config.inc.php");
     
     header('Content-Type: text/html; charset=utf-8');
+
+    if (isset($CONFIG['php_session_timeout']) && $CONFIG['php_session_timeout'] > 0) {
+        // Set the maxlifetime of session
+        ini_set("session.gc_maxlifetime",  $CONFIG['php_session_timeout']);
+  
+        // Also set the session cookie timeout to 0 (until the browser is closed)
+        ini_set("session.cookie_lifetime", 0);
+    }
+   
     session_start();
 
     if (!isset($_SESSION['login']) || ! $_SESSION['login']) {
