09d4b72f4ab7300f5a3a6d9a39486a5c2630f456
bilo1967 <17109395+bilo1967@users.noreply.github.com>
2021-09-25 17:07:44 +0200

    Update login.php

--- login.php
+++ login.php
@@ -112,6 +112,7 @@ if (@$_SESSION['login']) {
   <meta http-equiv="Content-type" content="text/html; charset=utf-8" />
 
   <meta name="description" content="ReBooth (REmote BOOTH) is a WebRTC based platform for conference interpreter training">
+  <meta name="keywords" content="interpreter,interpreters,interpreting,interpretation,simultaneous,consecutive,training,webrtc,conference,conference interpretation,interpretation booth">  
 
   <title>ReBooth:login</title>
 
