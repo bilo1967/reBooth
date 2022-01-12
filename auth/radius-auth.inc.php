c69bc05f638a87e44cfdf7e2847687d7300a3da3
bilo1967 <17109395+bilo1967@users.noreply.github.com>
2022-01-12 11:47:49 +0100

    Try authentication on several servers in cascade

--- auth/radius-auth.inc.php
+++ auth/radius-auth.inc.php
@@ -1,15 +1,55 @@
 <?php
 
-const MyRadiusServerAddress = "127.0.0.1";
-const MyRadiusSharedSecret  = "this is your shared secret";
-const MyRadiusTimeout       = 2;
-const MyRadiusRerty         = 1;
-const MyRadiusPort          = 0; // 0=auto
+const RadiusTimeout     = 2; // seconds
+const RadiusRetry       = 1;
+const RadiusDefaultPort = 0; // 0=auto
+
+const RadiusServers = [
+    [ 
+        "address" => "192.168.1.33",
+        "secret"  => "avada kedavra",
+        "port"    => RadiusDefaultPort
+    ],
+    [ 
+        "address" => "192.168.1.34",
+        "secret"  => "open sesame",
+        "port"    => RadiusDefaultPort
+    ],
+    [ 
+        "address" => "192.168.1.35",
+        "secret"  => "drakaris",
+        "port"    => RadiusDefaultPort
+    ],
+];
 
 function radiusAuth($u, $p) {
+    // Try all servers and throw an exception if they all fail
+    for ($i = 0; $i < count(RadiusServers); $i++) {
+        $isLast = ($i == count(RadiusServers) - 1);
+        $server = RadiusServers[$i];
+        try {
+            $ok = radiusAuth0($u, $p, $server["address"], $server["port"], $server["secret"]);
+            return $ok;
+        } catch (Exception $e) {
+            
+            // Let's log server failure
+            error_log("Radius server " . $server['address'] . " failed with error '" . $e->getMessage() . "'");
+            
+            if ($isLast) {
+                error_log('All radius server have failed');
+                throw new Exception('All radius server have failed');
+            }
+            
+        }
+    }
+    return false;
+}
+
+
+function radiusAuth0($u, $p, $address, $port, $secret, $timeout = RadiusTimeout, $retry = RadiusRetry) {
 
     $radius = radius_auth_open();
-    radius_add_server($radius, MyRadiusServerAddress, MyRadiusPort, MyRadiusSharedSecret, MyRadiusTimeout, MyRadiusRerty);
+    radius_add_server($radius, $address, $port, $secret, $timeout, $retry);
     radius_create_request($radius, RADIUS_ACCESS_REQUEST);
     radius_put_attr($radius, RADIUS_USER_NAME, $u);
     radius_put_attr($radius, RADIUS_USER_PASSWORD, $p);
@@ -31,7 +71,7 @@ function radiusAuth($u, $p) {
         break;
     default: // Other error
         $retval = false;
-        throw new Exception('A RADIUS error has occurred: ' . radius_strerror($radius));
+        throw new Exception(radius_strerror($radius));
         break;
     }
     return $retval;
